9742dbe18487d639e02aa6c6b953c4a469ef1e20
Sheikh Mubashir <smubashir655@gmail.com>
2021-11-09 17:23:22 +0500

    with firebase

--- app.js
+++ app.js
@@ -2,10 +2,19 @@ const list = document.getElementById('list');
 
 function addItem() {
     const todoItme = document.getElementById('todo_items');
+    const database = firebase.database().ref('todos')
+    const key = database.push().key
+    const todo = {
+        value: todoItme.value,
+        key: key
+    }
+    database.push(todo)
+}
 
+firebase.database().ref('todos').on('child_added', function (data) {
     //creating li
     const li = document.createElement('li');
-    const liText = document.createTextNode(todoItme.value);
+    const liText = document.createTextNode(data.val().value);
     li.appendChild(liText);
 
     //creating delete button
@@ -18,24 +27,30 @@ function addItem() {
     var editBtnText = document.createTextNode('Edit');
     editBtn.appendChild(editBtnText);
     editBtn.setAttribute('onclick', 'editBtn(this)')
+
+
     li.appendChild(editBtn);
     li.appendChild(button);
     list.appendChild(li);
-    todoItme.value = ''
+    
 
 
-}
 
-function deleteItem(e) {
-    e.parentNode.remove()
-}
+    
+})
+todoItme.value = ''
 
-function deleteAll() {
-    list.innerHTML = ''
-}
 
-function editBtn(e) {
-    const val = e.parentNode.firstChild.nodeValue;
-    const editValue = prompt('Enter Edit Value', val);
-    e.parentNode.firstChild.nodeValue = editValue;
-}
+// function deleteItem(e) {
+//     e.parentNode.remove()
+// }
+
+// function deleteAll() {
+//     list.innerHTML = ''
+// }
+
+// function editBtn(e) {
+//     const val = e.parentNode.firstChild.nodeValue;
+//     const editValue = prompt('Enter Edit Value', val);
+//     e.parentNode.firstChild.nodeValue = editValue;
+// }
